75aa3f33043fb14c9d385ab5eff44d31859d73bf
funnydman <crazy-dzmitry@mail.ru>
2020-08-06 15:19:02 +0300

    react app

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,56 @@
 import os
 
+import psycopg2
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
+from starlette.middleware.cors import CORSMiddleware
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIR = os.path.dirname(BASE_DIR)
 
 STATIC_DIR = os.path.join(PROJECT_DIR, 'static')
 
+main_router = APIRouter()
+
+DATABASE = {
+    'dbname': 'app',
+    'user': 'postgres',
+    'host': 'db',
+    'password': 'postgres'
+
+}
+
+conn = psycopg2.connect(**DATABASE)
+
+
+@main_router.get('/api/v1/get-data')
+def root():
+    with conn.cursor() as curr:
+        curr.execute("select * from data_src;")
+        return curr.fetchall()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost/api/"
+]
+
 
 def get_application() -> FastAPI:
     application = FastAPI()
+    application.include_router(main_router)
+    application.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     return application
 
 
 app = get_application()
-
-
-@app.post('/hello/{name}')
-def say_hello(name):
-    return f"Hello {name}"
-
 
 if __name__ == '__main__':
     uvicorn.run('main:app', port=8000, host='0.0.0.0', debug=True, reload=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                Hello!
-            </header>
-        </div>
-    );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mock_data: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost/api/v1/get-data', {
+                headers: {'Access-Control-Allow-Origin': '*'}
+            }
+        ).then(res => {
+            this.setState({mock_data: res.data});
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    {this.state.mock_data.map(obj => {
+                        return <p>{obj}</p>;
+                    })}
+                </header>
+            </div>
+        );
+    }
+
 }
 
 export default App;
